18e9a5cf033a52a32c24a552c9a874a1f6a14c72
harris <sgiri7598@gmail.com>
2022-08-06 14:02:53 +0530

    changes done

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ app.get("/", function(req, res) {
 });
 app.post("/", function(req, res) {
 
-  const query = req.body.cityname;
+  const query = req.body.cityName;
   const apikey = "a84e716da5ecbad7473f0c693351cf0f"
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=metric#"
+  const unit = "metric"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + unit;
+
   https.get(url, function(response) {
     console.log(response.statusCode);
     response.on("data", function(data) {
@@ -20,10 +22,10 @@ app.post("/", function(req, res) {
       const temp = wd.main.temp
       const wdd = wd.weather[0].description
       const icon  = wd.weather[0].icon
-      const imageUrl = "https://openweathermap.org/img/wn/10d@2x.png"
+      const imageUrl = "https://openweathermap.org/img/wn/" + icon +  "@2x.png"
       console.log(wd);
-      res.write("<h1> " + query + " the temp is " + temp + " degree C </h1>")
-      res.write("<h2> the weather is " + wdd + "  </h2>")
+      res.write("<h1> The temperature in " + query + "  is " + temp + " degree Celcius </h1>")
+      res.write("<h2> the weather is currently " + wdd + "  </h2>")
       res.write("<img src=" + imageUrl + ">")
       res.send()
     })
