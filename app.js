0ee3530440e7018faa0e02c39ba2e17674a73c4b
harris <sgiri7598@gmail.com>
2022-08-07 15:56:12 +0530

    changes done

--- app.js
+++ app.js
@@ -1,44 +1,160 @@
-const express = require("express")
-const app = express();
-const bodyparser = require("body-parser")
-app.use(bodyparser.urlencoded({
-  extended: true
-}))
-const https = require("https")
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html")
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request  = require('request');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}))
+
+// default city
+var city = 'kolkata';
+app.use(bodyParser.json());
+
+
+// rendering home page
+
+app.get('/',function (req,res) {
+  var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=76c8fdc8db07e476c13e988cb7a5690d`;
+
+  request(url , function (error , respone , body) {
+   var  weather_json =   JSON.parse(body);
+      var datetime = new Date().toDateString();
+
+  // console.log(weather_json);
+
+      var data = {
+          city: city,
+          temp: Math.round(weather_json.main.temp),
+          icon: weather_json.weather[0].icon,
+          des: weather_json.weather[0].description,
+          humi: weather_json.main.humidity,
+          country: weather_json.sys.country,
+          wind: weather_json.wind.speed,
+          temp_max: weather_json.main.temp_max,
+          pressure: weather_json.main.pressure,
+          date: datetime
+
+      }
+      var data = { data: data }
+      res.render('weather.ejs', data);
+  });
+
 });
-app.post("/", function(req, res) {
 
-  const query = req.body.cityName;
-  const apikey = "a84e716da5ecbad7473f0c693351cf0f"
-  const unit = "metric"
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + unit;
+// post request from search input from weather.ejs
 
-  https.get(url, function(response) {
-    console.log(response.statusCode);
-    response.on("data", function(data) {
-      const wd = JSON.parse(data)
-      const temp = wd.main.temp
-      const wdd = wd.weather[0].description
-      const icon  = wd.weather[0].icon
-      const imageUrl = "https://openweathermap.org/img/wn/" + icon +  "@2x.png"
-      console.log(wd);
-      res.write("<h1> The temperature in " + query + "  is " + temp + " degree Celcius </h1>")
-      res.write("<h2> the weather is currently " + wdd + "  </h2>")
-      res.write("<img src=" + imageUrl + ">")
-      res.send()
-    })
-
-  })
+app.post('/weather', (req,res)=>{
+  var city  = req.body.city2;
+  res.redirect('/weather2/'+city);
 })
 
 
-
-
-
-
-
-app.listen( process.env.PORT  || 3000, function() {
-  console.log("server running on port 3000");
+// post request from the celsius page
+app.post('/weather2', (req,res)=>{
+  var city = req.body.city;
+  res.redirect('/weather2/'+ city);
 })
+
+
+// finding data of the searched
+
+app.get('/weather2/:city', function (req, res) {
+  var city  = req.params.city;
+
+
+  var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=76c8fdc8db07e476c13e988cb7a5690d`;
+
+  request(url, function (error, respone, body) {
+      var weather_json = JSON.parse(body);
+      //console.log(weather_json);
+
+
+
+      if (!weather_json.message)
+      {
+          var datetime = new Date().toDateString();
+          var cel = ((Math.round(weather_json.main.temp) - 32) * 5) / 9;
+
+          var data = {
+              city: weather_json.name,
+              temp: Math.round(weather_json.main.temp),
+              icon: weather_json.weather[0].icon,
+              des: weather_json.weather[0].description,
+              humi: weather_json.main.humidity,
+              country : weather_json.sys.country,
+              wind: weather_json.wind.speed,
+              temp_max: weather_json.main.temp_max,
+              pressure: weather_json.main.pressure,
+              date: datetime
+
+          }
+          var data = { data: data }
+          res.render('weather.ejs', data);
+      }else{
+
+          res.render('weather.ejs', { data : "city not found" });
+      }
+
+
+  });
+
+});
+
+// taking city name to convert into celsius
+
+app.post('/cel', function (req, res) {
+   var city = req.body.city;
+
+   res.redirect('/cel/'+ city)
+
+});
+
+// converting to celsius
+app.get('/cel/:city', function (req, res) {
+  var city = req.params.city;
+  //console.log(city);
+
+  var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=76c8fdc8db07e476c13e988cb7a5690d`;
+
+  request(url, function (error, respone, body) {
+      var weather_json = JSON.parse(body);
+      //console.log(weather_json);
+
+
+
+
+      if (!weather_json.message) {
+          var datetime = new Date().toDateString();
+          var cel = ((Math.round(weather_json.main.temp) - 32) * 5) / 9;
+          var max = ((Math.round(weather_json.main.temp_max) - 32) * 5) / 9;
+          var km = ((weather_json.wind.speed) * 1.6093440)
+          var kmp = km.toFixed(3);
+
+          var data = {
+              city: weather_json.name,
+              temp: Math.round(cel),
+              icon: weather_json.weather[0].icon,
+              des: weather_json.weather[0].description,
+              humi: weather_json.main.humidity,
+              country: weather_json.sys.country,
+              wind: kmp,
+              temp_max: Math.round(max),
+              pressure: weather_json.main.pressure,
+              date: datetime,
+              cel : "yes"
+
+          }
+          var data = { data: data }
+          res.render('weather.ejs', data);
+      } else {
+          res.render('weather.ejs', { data: "city not found" });
+
+      }
+
+
+  });
+
+});
+
+
+// port to run
+app.listen(process.env.PORT || 3000);
